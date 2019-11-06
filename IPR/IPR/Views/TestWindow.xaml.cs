aaf7efa0354055cfae2951d66c40bfcc57016228
Lavictus <mark2baaij@gmail.com>
2019-11-06 22:48:15 +0100

    Added Check for input at start window

--- IPR/IPR/Views/TestWindow.xaml.cs
+++ IPR/IPR/Views/TestWindow.xaml.cs
@@ -20,12 +20,28 @@ namespace IPR
     /// </summary>
     public partial class TestWindow : Page
     {
+
+        public enum Sex
+        {
+            Male, Female, Other
+        }
+
         private delegate void OneArgDelagate(object arg);
+        private int patientID;
+        private int age;
+        private int weight;
+        private int ergoID;
+        private Sex sex;
 
-
-        public TestWindow()
+        public TestWindow(int patientID, int age, int weight, int ergoID, Sex sex)
         {
             InitializeComponent();
+
+            this.patientID = patientID;
+            this.age = age;
+            this.weight = weight;
+            this.ergoID = ergoID;
+            this.sex = sex;
 
             AstrandTest.AstrandTest at = new AstrandTest.AstrandTest();
             OneArgDelagate fetcher = new OneArgDelagate(at.StartTest);

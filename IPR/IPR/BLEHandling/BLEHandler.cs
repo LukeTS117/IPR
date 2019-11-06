01ab8148de18177eac724c72b2fa9d606e59f488
Luke Taylor <luketaylor001@gmail.com>
2019-11-06 22:01:16 +0100

    added client + Read/Write file

--- IPR/IPR/BLEHandling/BLEHandler.cs
+++ IPR/IPR/BLEHandling/BLEHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace IPR.BLEHandling
     class BLEHandler
     {
         public BLEConnect bleConnection;
+        string dir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
         public BLEHandler(BLEConnect bleConnection)
         {
@@ -23,6 +25,8 @@ namespace IPR.BLEHandling
             {
                 int heartRate = rawData[1];
                 Console.WriteLine("Heartrat: " + heartRate);
+                this.WriteToFile("<HR>" + heartRate);
+
             }
             else if(rawData[0] == 164)
             {
@@ -34,8 +38,59 @@ namespace IPR.BLEHandling
                 {
                     int instanteousCadence = message[2];
                     Console.WriteLine("instantaneos cadence:" + instanteousCadence);
+                    this.WriteToFile("<IC>" + instanteousCadence);
                 }
             }
         }
+
+        public void WriteToFile(string message)
+        {
+            
+            string path = this.dir + @"\TestData.txt";
+
+            if (!File.Exists(path))
+            {
+                Directory.CreateDirectory(dir);
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+
+        public string ReadFile()
+        {
+
+            string path = this.dir + @"\TestData.txt";
+            string packet = "";
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (sr.ReadLine() != null)
+                    {
+                        packet += sr.ReadLine();
+                    }
+                    return packet;
+                }
+            }
+            else
+            {
+                Console.WriteLine("This file does not exist");
+                return "";
+            }
+
+
+        }
     }
+
 }
+

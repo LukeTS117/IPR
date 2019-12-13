44aef8b70b6692d48af930ab0c57b174f0b16b57
Luke Taylor <luketaylor001@gmail.com>
2019-12-13 20:16:44 +0100

    now with datatypes :D

--- IPR/IPR/DataTypes.cs
+++ IPR/IPR/DataTypes.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace IPR.BLEHandling
 {
-    class BLEDataTypes
+    public enum DataTypes
     {
+        HR,
+        IC
     }
+
 }

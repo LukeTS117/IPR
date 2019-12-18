e3b3d47dc3d21294371b379291c94c708f2bd5e4
Lavictus <mark2baaij@gmail.com>
2019-12-18 16:36:46 +0100

    Do This Work?

--- IPR/IPR/AstrandTest/AstrandTest.cs
+++ IPR/IPR/AstrandTest/AstrandTest.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace IPR.AstrandTest
 {
@@ -17,7 +18,7 @@ namespace IPR.AstrandTest
         private readonly IAstrandData data;
 
         private static int WARMING_UP_TIME = 5; //Time in seconds 
-        private static int MAIN_TEST_TIME = 5;
+        private static int MAIN_TEST_TIME = 20;
         private static int COOLING_DOWN_TIME = 5;
         //private static int EXTENDED_TEST_TIME = 5;
 
@@ -26,6 +27,9 @@ namespace IPR.AstrandTest
         private Stopwatch elapsedStopWatch = null;
         private List<DataPoint> dataPoints;
         private List<int> instantCadence;
+        private Timer myTimer;
+        private AstrandTestPhase nextPhase;
+        bool testStarted = false;
 
         private struct DataPoint
         {
@@ -49,10 +53,13 @@ namespace IPR.AstrandTest
             this.dataPoints = new List<DataPoint>();
             this.instantCadence = new List<int>();
             this.data.Connect(this);
+
+
         }
 
         public void StartTest()
         {
+            this.testStarted = true;
             ChangePhase(AstrandTestPhase.WARMING_UP);
         }
 
@@ -90,31 +97,14 @@ namespace IPR.AstrandTest
 
         private void WarmingUp()
         {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            while (sw.Elapsed.TotalSeconds < WARMING_UP_TIME)
-            {
-                //dostuff
-                //testWindow.UpdateUI("I cant believe that this works");
-            }
-            sw.Stop();
-            Console.WriteLine("StopWatch stopped at: " + sw.Elapsed.TotalSeconds);
-            Console.WriteLine("Warming Up completed, moving on to Main Test");
-            ChangePhase(AstrandTestPhase.MAIN_TEST);
-
+            SetTimer(WARMING_UP_TIME, AstrandTestPhase.MAIN_TEST);
         }
 
         private void MainTest()
         {
             instantCadence = new List<int>();
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            while (sw.Elapsed.TotalSeconds < MAIN_TEST_TIME)
-            {
-                //dostuff
-            }
-            sw.Stop();
-            Console.WriteLine("StopWatch stopped at: " + sw.Elapsed.TotalSeconds);
+
+            SetTimer(MAIN_TEST_TIME, AstrandTestPhase.COOLING_DOWN);
 
 
             Console.WriteLine("Instant Cadence Results: ");
@@ -126,21 +116,15 @@ namespace IPR.AstrandTest
 
 
             Console.WriteLine("Main Test completed, moving on to Cooling Down");
-            ChangePhase(AstrandTestPhase.COOLING_DOWN);
+            
         }
 
         private void CoolingDown()
         {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            while (sw.Elapsed.TotalSeconds < COOLING_DOWN_TIME)
-            {
-                //dostuff
-            }
-            sw.Stop();
-            Console.WriteLine("StopWatch stopped at: " + sw.Elapsed.TotalSeconds);
+            SetTimer(COOLING_DOWN_TIME, AstrandTestPhase.INACTIVE);
+           
             Console.WriteLine("Cooling Down completed, deactivating test");
-            ChangePhase(AstrandTestPhase.INACTIVE);
+          
         }
 
         private void ExtendedTest()
@@ -155,20 +139,30 @@ namespace IPR.AstrandTest
 
         // // // // // // // //
 
-        private void SetStopWatch(int minutes)
+       
+
+        private void SetTimer(int seconds, AstrandTestPhase nextPhase)
         {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            while (sw.Elapsed.TotalMinutes < minutes)
-            {
-                Console.WriteLine("Stopwatch at:" + sw.Elapsed.TotalSeconds);
-            }
-            sw.Stop();
-            
+            this.nextPhase = nextPhase;
+            myTimer = new Timer(seconds * 1000);
+            myTimer.Elapsed += OnTimedEvent;
+            myTimer.Enabled = true;
+            myTimer.Start();
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            ChangePhase(nextPhase);
         }
 
         public void OnDataAvailable(DataTypes dataType, int value)
         {
+            if (!testStarted)
+            {
+                StartTest();
+            }
+
+
             dataPoints.Add(new DataPoint(dataType, value, this.GetElapsedTime()));
             Console.WriteLine(dataType.ToString() + " " + value);
 
